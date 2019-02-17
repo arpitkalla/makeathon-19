75426a1edd2077638841e43229627e1afb0f9be8
arpitkalla <kalla.arpit@gmail.com>
2019-02-16 21:05:37 -0500

    Resolution

--- camera_pi.py
+++ camera_pi.py
@@ -7,7 +7,7 @@ from base_camera import BaseCamera
 class Camera(BaseCamera):
     @staticmethod
     def frames():
-        with picamera.PiCamera(resolution=(640,320)) as camera:
+        with picamera.PiCamera(resolution=(320,160)) as camera:
             # let camera warm up
             time.sleep(2)
 
