e0878f4d6f178dd66534095ae42daa58d78649c1
arpitkalla <kalla.arpit@gmail.com>
2019-02-16 20:43:22 -0500

    Adding

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, Response
-import picamera
+from camera_pi import Camera
 import io
 
 app = Flask(__name__)
@@ -7,23 +7,22 @@ cam = picamera.PiCamera()
 img_stream = io.BytesIO()
 @app.route('/')
 def index():
+    """Video streaming home page."""
     return render_template('index.html')
 
 def gen(camera):
+    """Video streaming generator function."""
     while True:
-        cam.capture(img_stream, 'jpeg')
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + img_stream + b'\r\n')
-
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        
 @app.route('/video_feed')
 def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag."""
     return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-@app.route('/move/<dire>')
-def profile(dire="aaaa"):
-    print(dire)
-    return ""
 
 
 if __name__ == '__main__':
-        app.run(host='0.0.0.0', debug=True, threaded=True)
+        app.run(host='0.0.0.0', debug=True, threaded=False)
