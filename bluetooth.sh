ee6c87e49901573c83dfb2a674ff60724d7ac153
arpitkalla <kalla.arpit@gmail.com>
2019-02-16 21:41:49 -0500

    Adding bt

--- bluetooth.sh
+++ bluetooth.sh
@@ -2,3 +2,4 @@ sudo bluetoothctl
 agent on
 default-agent
 pair 11:11:22:AB:C2:E7
+connect 11:11:22:AB:C2:E7
--- templates/bluetooth.sh
+++
@@ -1,5 +0,0 @@
-sudo bluetoothctl
-agent on
-default-agent
-pair 11:11:22:AB:C2:E7
-connect 11:11:22:AB:C2:E7
