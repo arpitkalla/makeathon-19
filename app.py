ddd5643297896da924871ebd121d723b71921c56
arpitkalla <kalla.arpit@gmail.com>
2019-02-16 20:27:19 -0500

    Removing cv2

--- app.py
+++ app.py
@@ -1,30 +1,22 @@
-from flask import Flask, render_template, Response 
-import cv2
-import socket 
-import io 
+from flask import Flask, render_template, Response
+from camera import Camera
+
 app = Flask(__name__)
-vc = cv2.VideoCapture(0) 
+
 @app.route('/')
 def index():
-    """Video streaming"""
-    # set_up_gpio()
     return render_template('index.html')
 
-def gen():
-    """Video streaming generator function."""
+def gen(camera):
     while True:
-        rval, frame = vc.read()
-        cv2.imwrite('t.jpg', frame)
+        frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + open('t.jpg', 'rb').read() + b'\r\n')
-
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
 @app.route('/video_feed')
 def video_feed():
-    """Video streaming route. Put this in the src attribute of an img tag."""
-    return Response(gen(),
+    return Response(gen(Camera()),
                     mimetype='multipart/x-mixed-replace; boundary=frame')
-
 @app.route('/move/<dire>')
 def profile(dire="aaaa"):
     print(dire)
