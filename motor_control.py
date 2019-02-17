099417a0ca53a7fb8854e1f7338ea8a6e678512a
Viren Shah <vvs24@cornell.edu>
2019-02-16 21:50:51 -0500

    Updated motor_control

--- motor_control.py
+++ motor_control.py
@@ -1,4 +1,6 @@
 import RPi.GPIO as GPIO          
+
+
 from time import sleep
 
 m1_in1 = 24
@@ -21,6 +23,7 @@ GPIO.output(m1_in2,GPIO.LOW)
 m1_p=GPIO.PWM(m1_en,100)
 m1_p.start(100)
 
+GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(m2_in1,GPIO.OUT)
 GPIO.setup(m2_in2,GPIO.OUT)
 GPIO.setup(m2_en,GPIO.OUT)
@@ -33,27 +36,52 @@ print("The default speed & direction of motor is LOW & Forward.....")
 print("r-run s-stop f-forward b-backward l-low m-medium h-high e-exit")
 print("\n")    
 
+def m1_forwards():
+  GPIO.output(m1_in1,GPIO.HIGH)
+  GPIO.output(m1_in2,GPIO.LOW)
+
+def m2_forwards():
+  GPIO.output(m2_in1,GPIO.HIGH)
+  GPIO.output(m2_in2,GPIO.LOW)
+
+def m1_backwards():
+  GPIO.output(m1_in1,GPIO.LOW)
+  GPIO.output(m1_in2,GPIO.HIGH)
+
+def m2_backwards():
+  GPIO.output(m2_in1,GPIO.LOW)
+  GPIO.output(m2_in2,GPIO.HIGH)
+
+def move_forwards():
+  m1_forwards()
+  m2_forwards()
+  print("forwards")
+
+def move_backwards():
+  m1_backwards()
+  m2_backwards()
+  print("backwards")
+
+def move_left():
+  # First motor forwards
+  m2_forwards()
+  print("left")
+  
+def move_right():
+  # First motor forwards
+  m2_forwards()
+  print("right")
+
 while(1):
 
-    x=raw_input()
+  x=raw_input()
     
-    if x=='r':
-        print("run")
-        if(temp1==1):
-         # First motor forwards
-         GPIO.output(m1_in1,GPIO.HIGH)
-         GPIO.output(m1_in2,GPIO.LOW)
-
-         # Second motor forwards
-         GPIO.output(m2_in1,GPIO.HIGH)
-         GPIO.output(m2_in2,GPIO.LOW)
-         print("forward")
-        else:
-         # First motor backwards
-         GPIO.output(m1_in1,GPIO.LOW)
-         GPIO.output(m1_in2,GPIO.HIGH)
-
-         # Second motor backwards 
-         GPIO.output(m2_in1,GPIO.LOW)
-         GPIO.output(m2_in2,GPIO.HIGH)
-         print("backward")
+  if x=='r':
+    print("run")
+    # if obstuction detected
+    if(GPIO.input(14) == 0):
+      move_forwards()
+      print("forward")
+    else:
+      print("bluetooth warning")
+      # bluetooth
---
+++ speech_input.py
@@ -0,0 +1,56 @@
+import speech_recognition as sr
+import pyaudio 
+from threading import Thread
+import requests
+
+url = "http://localhost:5000/move/"
+direction = "n"
+sentRequest = True
+
+
+"""
+listen to user statement in mic
+returns spoken words from user OR 
+returns empty string if source not detected
+"""
+def listen(r, mic):
+  with mic as source:
+    r.adjust_for_ambient_noise(source)
+    print("\n\n\nYou may begin talking:\n\n\n") #testing
+    audio = r.listen(source)
+  
+  try:
+    return r.recognize_google(audio)
+
+  except sr.UnknownValueError:
+    print ("What are you saying?") #testing
+    return ""
+
+
+class ListenThread(Thread):
+  def __init__(self):
+    Thread.__init__(self)
+
+  def run(self):
+    while True:
+      global direction, setnRequest
+      r = sr.Recognizer()
+      
+      ### opens microphone and takes speech from human to convert to text
+      mic = sr.Microphone(0)
+      spoken_text = listen(r, mic)
+      if (spoken_text == "start"):
+	direction = "r"
+	sentRequest = False
+      else:
+        print(spoken_text)
+
+            
+thread = ListenThread()
+thread.start()
+
+while True:
+  if direction != "" and sentRequest == False:
+    requests.get(url + direction)
+    
+
